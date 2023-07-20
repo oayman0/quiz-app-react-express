fadfc05ffebc0b02fe2ceb840bcebd8ed07d77f7
oayman0 <omaraymankhalaf@gmail.com>
2023-07-20 16:19:10 +0300

    'meta-tags-update'

--- react-app/src/config/apiConfig.js
+++ react-app/src/config/apiConfig.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://localhost:8000";
-// const API_BASE_URL = "https://quiz-app-d6o7.onrender.com";
+// const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "https://quiz-app-d6o7.onrender.com";
 
 export default API_BASE_URL;
 
